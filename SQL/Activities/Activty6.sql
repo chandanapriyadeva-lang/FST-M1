4a3a1e338426629c0f8663fc5399f44932085f84
chandanapriyadeva-lang <chandanapriya.deva@gmail.com>
2025-12-10 17:13:26 +0530

    Create Activty6.sql

---
+++ SQL/Activities/Activty6.sql
@@ -0,0 +1,4 @@
+UPDATE SALESMAN2 SET grade = 200 WHERE salesman_city = 'Rome';
+UPDATE SALESMAN2 SET grade = 300 WHERE salesman_name = 'James Hoog';
+UPDATE SALESMAN2 SET salesman_name = 'Pierre' WHERE salesman_name = 'McLyon';
+SELECT * FROM salesman;
