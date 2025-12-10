9966f6aa7b688027d381fc376f27982261dcfd14
chandanapriyadeva-lang <chandanapriya.deva@gmail.com>
2025-12-10 17:12:26 +0530

    Create Activity3.sql

---
+++ SQL/Activities/Activity3.sql
@@ -0,0 +1,3 @@
+SELECT salesman_id,salesman_city FROM SALESMEN2;
+SELECT  * FROM SALESMAN2 Where salesman_city = 'Paris';
+SELECT salesman_id,commission  FROM SALESMAN2 where salesman_name = 'Paul Adam';
